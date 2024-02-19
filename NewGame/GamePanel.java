caef50668f8fdc593106aed305ed079b95998449
Rishav <140265067+IamRishavDas@users.noreply.github.com>
2024-02-20 00:13:40 +0530

    Bad Game Loop Created with the FPS counter Check

--- NewGame/GamePanel.java
+++ NewGame/GamePanel.java
@@ -11,6 +11,10 @@ public class GamePanel extends JPanel {
     private int posX = 100;
     private int posY = 100;
 
+    //fps counter
+    private int frames = 0;
+    private long lastCheck = 0;
+
     public GamePanel() {
         addKeyListener(new KeyController(this));
         addMouseListener(new MouseController(this));
@@ -36,6 +40,14 @@ public class GamePanel extends JPanel {
         super.paintComponent(g);
 
         g.fillOval(posX, posY, 100, 100);
+
+        frames++;
+        if((System.currentTimeMillis() - lastCheck) >= 1000){
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
+
         repaint();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PinBall
 A simple Pin Ball game using java
+
+1. First Simple JFrame And JPanel for the game
+2. Draw basic components using the paintComponent defined in JPanel
+public void paintCompoent(Graphics g){
+    super.paintComponent(g);
+    ...// drawing methods ...
+}
+Also, update the paintComponet() using the repaint() method.
+3. Game Loop --->
+    Every Game must contain a game Loop 
+    1. repaint() game loop
+    2. make a fps counter
+    3. visuals
