11e2a9a479cd341703893b113653214135492f99
Rishav <140265067+IamRishavDas@users.noreply.github.com>
2024-02-20 00:39:04 +0530

    Random Ball Color Generator

--- NewGame/GamePanel.java
+++ NewGame/GamePanel.java
@@ -5,7 +5,9 @@ import javax.swing.JPanel;
 import Controller.KeyController;
 import Controller.MouseController;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
     private float posX = 50f;
@@ -18,6 +20,9 @@ public class GamePanel extends JPanel {
     // fps counter
     private int frames = 0;
     private long lastCheck = 0;
+
+    //ball color
+    private Color color = new Color(24,255,254);
 
     public GamePanel() {
         addKeyListener(new KeyController(this));
@@ -43,6 +48,7 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        g.setColor(color);
         g.fillOval((int)posX, (int)posY, 100, 100);
         updateOval();
 
@@ -59,13 +65,25 @@ public class GamePanel extends JPanel {
     private void updateOval() {
         
         posX += dirX;
-        if (posX > 900 || posX < 0)
+        if (posX > 900 || posX < 0){
             dirX *= -1;
+            color = getRandomColor();
+        }
 
         posY += dirY;
 
-        if (posY > 500 || posY < 0)
+        if (posY > 500 || posY < 0){
             dirY *= -1;
+            color = getRandomColor();
+        }
+    }
+
+    private Color getRandomColor(){
+        Random random = new Random();
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+        return new Color(r,g,b);
     }
 
 }
